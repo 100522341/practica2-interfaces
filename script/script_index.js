bedcd0410fa9bf0964b3c625d096fc84936b48fe
100522341 <100522341@alumnos.uc3m.es>
2025-10-06 16:27:41 +0200

    Arreglar las imágenes, cerrar body y creación scripts

---
+++ script/script_index.js
@@ -0,0 +1,5 @@
+/*  AQUÍ IRÁ TODO NUESTRO SCRIPT  (creo que es como
+el style.css, aunque en el enunciado pone SCRIPTS. Mejor si lo hacemos
+separado creo)*/
+
+/* HE creado 4 pero quizás sobre 1 (el de la versión idéntica a index.html)*/
