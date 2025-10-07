1850977c8777a9b0df64b5e42708894b78de9f51
100522341 <100522341@alumnos.uc3m.es>
2025-10-07 12:48:34 +0200

    Empezando con packs-viajes

--- script/script_index.js
+++ script/script_index.js
@@ -3,3 +3,8 @@ el style.css, aunque en el enunciado pone SCRIPTS. Mejor si lo hacemos
 separado creo)*/
 
 /* HE creado 4 pero quizás sobre 1 (el de la versión idéntica a index.html)*/
+
+// Seleccionamos los elementos
+const flechaIzquierda = document.querySelector(".flecha-izquierda");
+const flechaDerecha = document.querySelector(".flecha-derecha");
+const packsViajes = document.querySelector("packs-viajes");
