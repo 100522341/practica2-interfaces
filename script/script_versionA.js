2a6650484340af0197c917e4c77e49e22e58e510
100522300 <100522300@alumnos.uc3m.es>
2025-10-08 09:59:21 +0200

    version A

---
+++ script/script_versionA.js
@@ -0,0 +1,103 @@
+/* ----Seleccionamos los elementos del DOM---- */
+const form      = document.getElementById('formA');
+const politica  = document.getElementById('politica');
+const boton     = document.getElementById('btn-guardar');
+const nombre    = document.getElementById('nombre');
+const apellidos = document.getElementById('apellidos');
+const correo    = document.getElementById('correo');
+const correo2   = document.getElementById('correo2');
+const fecha     = document.getElementById('fecha');
+const login     = document.getElementById('login');
+const password  = document.getElementById('password');
+const imagen    = document.getElementById('imagen');
+
+
+/* ----Activación del boton---- */
+function activarBoton() { //Funcion que activa el boton guardar si esta marcada la casilla politica de privacidad
+  boton.disabled = !politica.checked;
+}
+politica.addEventListener('change', activarBoton);//Cada vez que el usuario marque o desmarque se ejcuta la funcion activar boton
+activarBoton();
+
+
+/* ----Impedir fechas no validas---- */
+(function establecerFechaMax(){
+  // Establece el atributo max de la fecha a YYYY-MM-DD de hoy (date obtiene la fecha,to string lo pasa a str,y slice obtiene la fecha que son solo los 10 primeros caracteres)
+  const fechaMax = new Date().toISOString().slice(0, 10);
+  if (fecha) {
+    fecha.setAttribute('max', fechaMax);
+  }
+})();
+
+
+/* ----Imagenes permitidas---- */
+imagen.addEventListener('change', () => { //Evento que se activa cuando el usuario añade el archivo
+  const archivo = imagen.files[0];//Archivo subido por el usuario
+  if (archivo && ['image/webp','image/png','image/jpeg'].includes(archivo.type)) return; //Si hay archivo subido y su tipo se permite no se muestra alerta,en caso contrario si
+  alert('Formato de imagen no adminitido');
+  imagen.value = '';//Si el formato no es valido borra el archibo subido
+});
+
+
+/* ----Correos iguales---- */
+function correosCoinciden() {
+  return correo.value.trim() === correo2.value.trim();//El crreo debe ser igual a "confirmar correo"
+}
+
+
+/* ----Requisitos para el submit---- */
+//Cuando el usuario interactua con el formulario no deja que lo envie hasta controlar todos los campos
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+
+  //Aceptar politica de privacidad
+  if (!politica.checked) {
+    alert('Debes aceptar la política de privacidad.');
+    return;
+  }
+
+
+  //Se encarga de que se cumplan los minlegth,pattern etc de version-A.html
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+
+  //Emails iguales
+  if (!correosCoinciden()) {
+    alert('Los correos no coinciden.');
+    correo2.focus();
+    return;
+  }
+
+
+  //Debe subirse unja imagen con el formato correcto
+  const archivo = imagen.files[0];
+  const fotoCorrecta = archivo && ['image/webp','image/png','image/jpeg'].includes(archivo.type);
+  if (!fotoCorrecta) {
+    alert('Formato de imagen no admitido');
+    return;
+  }
+
+
+  //Construimos un objeto usuario con los valores introducidos
+  const usuario = {
+    nombre:    nombre.value.trim(),//Obtiene el texto que el usuario escribio y con trim eliminamos espacios innecesarios
+    apellidos: apellidos.value.trim(),
+    correo:    correo.value.trim(),
+    login:     login.value.trim()
+  };
+
+
+  //Guardamos los datos del usuario en el localStorage
+  const usuarios = JSON.parse(localStorage.getItem('users') || '{}');//Carga los usuarios guardados o un JSON vacio si no hay
+  usuarios[usuario.login] = usuario;//Guarda o actualiza al usuario
+  localStorage.setItem('usuarios', JSON.stringify(usuarios));//Actualiza el localStorage con todos los usuarios
+  localStorage.setItem('sesion', JSON.stringify({ login: usuario.login }));//Guarda la sesion actual
+
+
+//Redirigimos a la version B
+  window.location.href = 'version-B.html';
+});
